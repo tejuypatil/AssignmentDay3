59c3ab2386774ac8f605226739adeee7ac2249a2
tejuypatil <tejuypatil@gmail.com>
2022-07-08 23:52:56 +0530

    Used switch case Statement

--- src/com/employee/wage/EmpWage.java
+++ src/com/employee/wage/EmpWage.java
@@ -11,23 +11,24 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation");
 
-        int dailyEmpWage;
-        int randomCheck = (int) Math.round(random()*2);
+        int dailyEmpWage ;
+        int randomCheck = (int) Math.round(random() * 2);
         int partTimeEmpWage;
-        if (1 == randomCheck)
-        {
-            System.out.println("Employee is present Full time");
-            dailyEmpWage= WAGE_PER_HOUR * FULL_Day_HOUR;
-            System.out.println("Employee wage is :"+dailyEmpWage);
+
+        switch (randomCheck) {
+            case 1:
+                System.out.println("Employee is present Full time");
+                dailyEmpWage = WAGE_PER_HOUR * FULL_Day_HOUR;
+                System.out.println("Employee wage is :" + dailyEmpWage);
+                break;
+            case 2:
+                System.out.println("Employee is present Part Time");
+                partTimeEmpWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                System.out.println("Employee wage is :" + partTimeEmpWage);
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
-        else if (randomCheck > 1) {
-            System.out.println("Employee is present Part Time");
-            partTimeEmpWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-            System.out.println("Employee wage is :"+partTimeEmpWage);
-        }
-        else
-        {
-        System.out.println("Employee is absent");
-        }
+
     }
 }
