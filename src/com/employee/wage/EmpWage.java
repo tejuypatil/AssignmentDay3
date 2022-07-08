37132c1b1a583aac0c1c70789ecd3f1208d0120a
tejuypatil <tejuypatil@gmail.com>
2022-07-08 17:30:28 +0530

    Added Program to find Daily Wage of Employee

--- src/com/employee/wage/EmpWage.java
+++ src/com/employee/wage/EmpWage.java
@@ -3,13 +3,22 @@ package com.employee.wage;
 import static java.lang.Math.random;
 
 public class EmpWage {
+
+    final static int WAGE_PER_HOUR = 20;
+    final static int FULL_Day_HOUR = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation");
+
+        int dailyEmpWage=0;
         int randomCheck = (int) Math.round(random());
+
         if (1 == randomCheck) {
             System.out.println("Employee is present");
+            dailyEmpWage = WAGE_PER_HOUR * FULL_Day_HOUR;
         } else {
             System.out.println("Employee is absent");
         }
+        System.out.println("Employee wage per day is :" + dailyEmpWage);
     }
 }
