ffe8dbbdfc23ad66fdb4c0be2b444a24893ff10c
tejuypatil <tejuypatil@gmail.com>
2022-07-09 20:17:13 +0530

    Added welcome msg and player position

---
+++ src/com/snakeladder/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.snakeladder;
+
+public class SnakeLadder {
+
+    public static int position=0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake & Ladder:\n" +
+                "            _ _ _ _ _ _\n" +
+                "~~~~====:>  _|_|_|_|_|_\n\n");
+        System.out.println("Player : 웃 at " +position+" position");
+    }
+}
